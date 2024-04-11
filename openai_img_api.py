34872a2645926514914ddb68219bd5c06a665f87
workszop <75222087+workszop@users.noreply.github.com>
2024-04-11 05:45:00 +0200

    Create openai_img_api.py

---
+++ openai_img_api.py
@@ -0,0 +1,53 @@
+import os
+from PIL import Image
+import base64
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+api_key = os.getenv('OPENAI_KEY')
+
+# Function to encode the image
+def encode_image(image_path):
+  with open(image_path, "rb") as image_file:
+    return base64.b64encode(image_file.read()).decode('utf-8')
+
+# Path to your image
+image_path = input('podaj ścieżkę do pliku\n')
+Image.open(image_path).show()
+
+prompt = input('\nw czym mogę pomóc?\n')
+
+# Getting the base64 string
+base64_image = encode_image(image_path)
+
+headers = {
+  "Content-Type": "application/json",
+  "Authorization": f"Bearer {api_key}"
+}
+
+payload = {
+  "model": "gpt-4-turbo",
+  "messages": [
+    {
+      "role": "user",
+      "content": [
+        {
+          "type": "text",
+          "text": prompt
+        },
+        {
+          "type": "image_url",
+          "image_url": {
+            "url": f"data:image/jpeg;base64,{base64_image}"
+          }
+        }
+      ]
+    }
+  ],
+  "max_tokens": 300
+}
+
+response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
+
+print(response.json()['choices'][0]['message']['content'])
